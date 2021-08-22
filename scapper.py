3d947151d8a05042b419628381a24b818213c5ad
naeem-bebit <naeemhussien@gmail.com>
2021-08-22 22:55:46 +0800

    droplevel

--- scapper.py
+++ scapper.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import string
 
-df_append = pd.DataFrame()
+df_main = pd.DataFrame()
 url = 'https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_'
 for i in list(string.ascii_uppercase):
     list_url = url+i
     df = pd.read_html(list_url)[0]
-    df_append = df_append.append(df)
+    df.columns = df.columns.droplevel(1)
+    df_main = df_main.append(df)
