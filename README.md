1fde2b0a5d7f5f702f046bb8011c4d357333abee
naeem-bebit <naeemhussien@gmail.com>
2021-09-08 22:36:53 +0800

    update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Iata with timezone data
 
 This data is combination of iata data with timezone data
-Scrapped from the https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_A 
+Scrapped from the https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_
--- scapper.py
+++ scapper.py
@@ -6,5 +6,5 @@ url = 'https://en.wikipedia.org/wiki/List_of_airports_by_IATA_airport_code:_'
 for i in list(string.ascii_uppercase):
     list_url = url+i
     df = pd.read_html(list_url)[0]
-    df.columns = df.columns.droplevel(1)
+    df.columns = df.columns.droplevel(1) #dfb.droplevel(1, axis=1)
     df_main = df_main.append(df)
